98dfcaa0179d5fd0436a927fcd04f78ebd0ba984
Stanley Huang <huang.stanley050@gmail.com>
2020-02-08 13:26:18 +1100

    started with User model

---
+++ api/models/User.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const Schema = mongoose.Schema();
+
+const userSchema = new Schema({
+  email: String,
+  password: String
+});
+userSchema.methods.comparePassword = (userPassword, cb) => {
+  bcrypt.compare(userPassword, password, (err, isMatch) => {
+    cb(err, isMatch);
+  });
+};
+const User = mongoose.model("user", userSchema);
+module.exports = User;
